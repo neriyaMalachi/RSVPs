b450dd16f8183c07cdb9b7efe3ed04238ee5fa19
Neriya Malachi <neriamalachi@gmail.com>
2024-09-08 20:38:43 +0300

    ..

--- app/(server)/api/guests/route.ts
+++ app/(server)/api/guests/route.ts
@@ -4,16 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 import sendRegistrationSuccessEmail from "../../nodemailer/SendMail";
 require("@/app/(server)/models/Guest");
 
-
 // פונקציה להוספת כותרות CORS
-function setCORSHeaders(res: NextResponse) {
-  res.headers.set("Access-Control-Allow-Origin", "*");
-  res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-}
 
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
@@ -21,44 +12,34 @@ export async function GET(req: NextRequest, res: NextResponse) {
     const guests = await Guest.find();
 
     return NextResponse.json({ success: true, guests });
-    // setCORSHeaders(response); // הוספת כותרות CORS לתגובה
-    // return response;
   } catch (error: any) {
     return NextResponse.json({
       success: false,
       status: 400,
       message: error.message,
     });
-    // setCORSHeaders(response); // הוספת כותרות CORS לתגובה
-    // return response;
   }
 }
 
 export async function POST(req: NextRequest, res: NextResponse) {
   const data = await req.json();
   console.log(data);
-  
+
   // sendRegistrationSuccessEmail(data.email);
   try {
     await dbConnect();
     const existingGuest = await Guest.findOne({ email: data.email });
     if (existingGuest) {
-      const response = NextResponse.json({
+      return NextResponse.json({
         status: 400,
         message: "Guest with this email already exists",
       });
-      setCORSHeaders(response); // הוספת כותרות CORS לתגובה
-      return response;
     }
 
     await Guest.create(data);
-    const response = NextResponse.json({ status: 201, success: true });
-    setCORSHeaders(response); // הוספת כותרות CORS לתגובה
-    return response;
+    return NextResponse.json({ status: 201, success: true });
   } catch (error: any) {
-    const response = NextResponse.json({ message: error.message, status: 404 });
-    setCORSHeaders(response); // הוספת כותרות CORS לתגובה
-    return response;
+    return NextResponse.json({ message: error.message, status: 404 });
   }
 }
 
@@ -79,17 +60,15 @@ export async function PUT(req: NextRequest, res: NextResponse) {
       }
     );
 
-    const response = NextResponse.json({
+    return NextResponse.json({
       message: "successfull",
       status: 200,
       data: NewGusts,
     });
-    setCORSHeaders(response); // הוספת כותרות CORS לתגובה
-    return response;
+   
   } catch (error: any) {
-    const response = NextResponse.json({ message: error.message, status: 400 });
-    setCORSHeaders(response); // הוספת כותרות CORS לתגובה
-    return response;
+    return NextResponse.json({ message: error.message, status: 400 });
+    
   }
 }
 
@@ -100,23 +79,20 @@ export async function DELETE(req: NextRequest, res: NextResponse) {
     const result = await Guest.findByIdAndDelete(DeleteGusts.id);
 
     if (!result) {
-      const response = NextResponse.json({
+      return NextResponse.json({
         message: "Guest not found",
         status: 404,
       });
-      setCORSHeaders(response); // הוספת כותרות CORS לתגובה
-      return response;
+     
     }
 
-    const response = NextResponse.json({
+    return NextResponse.json({
       message: "Deleted Successfull",
       status: 200,
     });
-    setCORSHeaders(response); // הוספת כותרות CORS לתגובה
-    return response;
+   
   } catch (error: any) {
-    const response = NextResponse.json({ message: error.message, status: 400 });
-    setCORSHeaders(response); // הוספת כותרות CORS לתגובה
-    return response;
+    return NextResponse.json({ message: error.message, status: 400 });
+   
   }
 }
